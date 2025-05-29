2e464b185d692b352e1da94e5aeef55ac4deda27
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-05-29 20:40:28 +0200

    finish test with github link repo

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,11 +2,12 @@ import {screen, render} from '@testing-library/react';
 import RepositoriesListItem from './RepositoriesListItem';
 import {MemoryRouter} from "react-router";
 
-jest.mock('../tree/FileIcon', () => {
-    return () => {
-        return 'FileIcon Component';
-    }
-});
+// jest.mock('../tree/FileIcon', () => {
+//     return () => {
+//         return 'FileIcon Component';
+//     }
+// });
+// Mocking the FileIcon component to avoid external dependencies
 
 function renderComponent() {
     const repository = {
@@ -17,20 +18,28 @@ function renderComponent() {
         name: 'repo-name',
         html_url: 'https://github.com/repo-name'
     };
-    return render(
+
+    render(
         <MemoryRouter>
-            <RepositoriesListItem repository={repository}/>
-        </MemoryRouter>);
+            <RepositoriesListItem repository={repository} />
+        </MemoryRouter>
+    );
+
+    return { repository };
 }
 
 test('shows a link to the github homepage for this repository', async () => {
-    renderComponent();
+    const {repository} = renderComponent();
 
-    // await screen.findByRole('img', {name: 'JavaScript'}); - only used if FileIcon component was not mocked
+    await screen.findByRole('img', {name: 'JavaScript'});
+
+    const link = screen.getByRole('link', {name: /github repository/i});
+    expect(link).toHaveAttribute('href', repository.html_url);
 })
 
-const pause = () => {
-    return new Promise(resolve => setTimeout(() => {
-        resolve();
-    }, 100));
-}
+// const pause = () => {
+//     return new Promise(resolve => setTimeout(() => {
+//         resolve();
+//     }, 100));
+// }
+// Function to pause execution for a short time (used for debugging purposes)
